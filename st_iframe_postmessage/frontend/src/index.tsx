7aef72abd0a53642c9378cea2aec54a276745864
Piotr Synowiec <psynowiec@gmail.com>
2024-12-05 12:54:31 +0100

    logs: done

--- st_iframe_postmessage/frontend/src/index.tsx
+++ st_iframe_postmessage/frontend/src/index.tsx
@@ -5,10 +5,14 @@ function onRender(event: Event): void {
     const data = (event as CustomEvent<RenderData>).detail;
     const message = data.args['message'];
     const targetOrigin = data.args['target_origin'];
+    const consoleLog = data.args['console_log'];
 
     const wtop = window.top
     if (wtop) {
         wtop.postMessage(message, targetOrigin)
+    }
+    if (consoleLog) {
+        console.log(`[${(new Date().toISOString())}] message : ${safeStringify(message)}, targetOrigin : ${targetOrigin}`)
     }
 }
 
@@ -17,3 +21,28 @@ Streamlit.setComponentReady()
 Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender)
 
 
+function safeStringify(variable: any): string {
+    if (variable === null) {
+        return 'null';
+    } else if (variable === undefined) {
+        return 'undefined';
+    } else if (typeof variable === 'string') {
+        return variable;
+    } else if (typeof variable === 'number' || typeof variable === 'boolean') {
+        return variable.toString();
+    } else if (Array.isArray(variable)) {
+        return JSON.stringify(variable);
+    } else if (typeof variable === 'object') {
+        try {
+            return JSON.stringify(variable);
+        } catch (error) {
+            if (error instanceof Error) {
+                return `Unable to stringify object: ${error.message}`;
+            } else {
+                return `Unable to stringify object: ${String(error)}`;
+            }
+        }
+    } else {
+        return 'Unknown type';
+    }
+}
